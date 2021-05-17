75278b8a3941f925353b120b7162f3272c0ba22c
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-17 01:26:27 -0300

    Add 2015 day 04

---
+++ 2015/day 04/partOne.py
@@ -0,0 +1,13 @@
+import hashlib
+
+questionInput = "bgvyzdsv"
+test = 0
+
+while 1:
+    testInput = questionInput + str(test)
+
+    if hashlib.md5(testInput.encode()).hexdigest().startswith("00000"):
+        print(test)
+        break
+
+    test += 1
