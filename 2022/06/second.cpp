0bde77ae2df4e8cda2bdd4b2e690fff15895a78c
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-12-06 13:39:24 -0300

    Add day six of the year 2022

---
+++ 2022/06/second.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    string s;
+
+    cin >> s;
+
+    unordered_map<char, int> letters;
+    int p1 {}, p2 {}, current {};
+
+    while (p1 < s.size() && p2 < s.size()) {
+        char ch {s[p2++]};
+
+        ++letters[ch];
+        ++current;
+
+        if (letters[ch] != 1) {
+            while (letters[ch] != 1) {
+                char current_ch {s[p1++]};
+
+                --letters[current_ch];
+                --current;
+            }
+        }
+
+        if (current == 14) {
+            break;
+        }
+    }
+
+    cout << p2 << '\n';
+
+    return 0;
+}
